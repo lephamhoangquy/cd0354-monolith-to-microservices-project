8521230463401c5b7372ccdef006f0228a00a3ac
quylph <quylph@vng.com.vn>
2023-07-22 18:14:20 +0700

    update config

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=quyle00000
-export POSTGRES_HOST=hang-database-007.cp7jj5g7owjy.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=hang-4162fa78-db.czdcfwaemris.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=hang-007-dev
+export AWS_BUCKET=hang-4162fa78
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
